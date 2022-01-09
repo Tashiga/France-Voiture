4e643eaac43c038d468836a74eb692d8e67a4cd0
Natxue <natachaf.xue@gmail.com>
2022-01-09 18:48:30 +0100

    modification en MVC

---
+++ site/controleur/inscription.php
@@ -0,0 +1,21 @@
+<?php
+require_once("../modele/Inscription.php");
+//require_once("../vue/inscription.php");
+
+$modele_inscription = new Inscription();
+
+//detecte lorsque l'internaute clique sur le bouton pour s'inscrire
+if($_POST) {
+    $fonction_sql->debug($_POST);
+
+    $message_erreur = $modele_inscription->formulaire_valide();
+    if($message_erreur != NULL){
+        echo $message_erreur;
+    }
+    else{
+        $modele_inscription->s_inscrire();
+        echo "Inscrption réussie";
+    }
+}
+
+?>
--- site/vue/inscription.php
+++ site/vue/inscription.php
@@ -1,37 +1,4 @@
-<?php
-require_once("../inc/initialisation.php");
-
-//detecte lorsque l'internaute clique sur le bouton pour s'inscrire
-if($_POST)
-{
-    $fonction_sql->debug($_POST);
-
-	if (empty($_POST["email"])) {
-		$message = "<div class='erreur'>Entrez une adresse email valide.</div>";
-	}
-    else {
-        $table = $_POST['VendeurAcheteur'];
-        $membre = $fonction_sql->executeRequete("SELECT * FROM $table WHERE email='$_POST[email]'");
-        if($membre->num_rows > 0) {
-            $message= "<div class='erreur'>Vous avez déjà créé un compte avec cette adresse email.</div>";
-        }
-        else {
-            $_POST['mdp'] = md5($_POST['mdp']);
-            foreach($_POST as $indice => $valeur)
-            {
-                $_POST[$indice] = htmlEntities(addSlashes($valeur));
-            }
-            $fonction_sql->executeRequete("INSERT INTO $table(pseudo, email, mdp) VALUES ('$_POST[pseudo]', '$_POST[email]', '$_POST[mdp]')");
-            $message= "<div class='erreur'>Inscription réussie.</div>";
-        }
-    }
-    echo $message;
-}
-
-
-//--------------------------------- AFFICHAGE HTML ---------------------------------//
-require_once("../inc/haut_site.php");  
-?>
+<?php require_once("../inc/haut_site.php"); ?>
 
 <main>
 
@@ -48,7 +15,7 @@ require_once("../inc/haut_site.php");
             <input type="password" id="mdp" name="mdp" required="required"><br><br>
 
             <label for="mdp">Confirmation mot de passe</label><br>
-            <input type="password" id="confmdp" name="mdp" required="required"><br><br>
+            <input type="password" id="confmdp" name="confmdp" required="required"><br><br>
                 
             <label for="VendeurAcheteur">Vendeur ou Acheteur ?</label><br>
             <input type="radio" name="VendeurAcheteur" value="vendeur" checked="" >Je souhaite vendre<br>
