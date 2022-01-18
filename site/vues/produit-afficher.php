2c7efae7c31b1933ac60f687b4d007d2d8a93a34
Natxue <natachaf.xue@gmail.com>
2022-01-18 14:50:17 +0100

    MVC profil OK

--- site/vues/produit-afficher.php
+++ site/vues/produit-afficher.php
@@ -62,26 +62,7 @@ if(isset($_GET['action']) && $_GET['action'] == "afficher") {
 	<?php
 }
 
-//supprimer un article
-if(isset($_GET['action']) && $_GET['action'] == "suppression") {   
-    // $contenu .= $_GET['id_produit']
-    $resultat = $fonction_sql->executeRequete("SELECT * FROM article WHERE idArticle=$_GET[id_article]");
-    //Recupere l'id du photo
-    $produit_a_supprimer = $fonction_sql->executeRequete("SELECT * FROM affiche WHERE idArticle =$_GET[id_article]");
-    while($autre = $produit_a_supprimer->fetch_assoc()) {
-        //supprime l'affichage
-        $fonction_sql->executeRequete("DELETE FROM affiche WHERE idArticle =$_GET[id_article]");
-        $chemin_photo_a_supprimer = $fonction_sql->executeRequete("SELECT * FROM photo WHERE idPhoto='".$autre['idPhoto']."'");
-        $autres = $chemin_photo_a_supprimer->fetch_assoc();
-        if(!empty($autre['idArticle']) && file_exists($autres['cheminPhoto'])) {
-            unlink($autres['cheminPhoto']);
-        }
-        $fonction_sql->executeRequete("DELETE FROM photo WHERE idPhoto='".$autre['idPhoto']."'");
-        $fonction_sql->executeRequete("DELETE FROM ajouter WHERE idArticle=$_GET[id_article]");
-        $fonction_sql->executeRequete("DELETE FROM article WHERE idArticle=$_GET[id_article]");
-        header("location:produit-afficher.php?action=afficher");
-    }
-}
+
 
 
 //modifier un article
@@ -170,70 +151,5 @@ if(isset($_GET['action']) && $_GET['action'] == "modification") {
         <?php
     }
 
-    //si vendeur veut modifier article
-    if($_POST) {
-        // $fonction_sql->debug($_FILES);
-        if(!empty($_POST['nom_article'])) {
-            $fonction_sql->executeRequete("update article set nom = '$_POST[nom_article]' where idArticle = $_GET[id_article]");
-            $cheminPhoto = $fonction_sql->executeRequete("SELECT cheminPhoto from photo where idPhoto = (SELECT idPhoto from affiche where idArticle = $_GET[id_article]) ");
-            $cheminPhoto = $cheminPhoto->fetch_assoc();
-            $extension_photo =  substr($cheminPhoto['cheminPhoto'], -4);
-            $newChemin = "../inc/img/photos_articles/$_POST[nom_article]$extension_photo";
-            if(rename($cheminPhoto['cheminPhoto'], $newChemin)) {
-                $fonction_sql->executeRequete("update photo set cheminPhoto = '$newChemin' where idPhoto =(SELECT idPhoto from affiche where idArticle = $_GET[id_article] )");
-                echo '<p class = validation>validation ok pour '.$_POST['nom_article'].'</p>';
-            }
-            
-        }
-        if(!empty($_POST['prix_article'])) {
-            $fonction_sql->executeRequete("update article set prix = '$_POST[prix_article]' where idArticle = $_GET[id_article]");
-            echo '<p class = validation>validation ok pour '.$_POST['prix_article'].'</p>';
-        }
-        if(!empty($_POST['description_article'])) {
-            $fonction_sql->executeRequete("update article set description = '$_POST[description_article]' where idArticle = $_GET[id_article]");
-            echo '<p class = validation>validation ok pour '.$_POST['description_article'].'</p>';
-        }
-        if(!empty($_POST['stock_article'])) {
-            $fonction_sql->executeRequete("update article set nom = '$_POST[stock_article]' where idArticle = $_GET[id_article]");
-            echo '<p class = validation>validation ok pour '.$_POST['stock_article'].'</p>';
-        }
-        if(!empty($_POST['categorie_article'])) {
-            $fonction_sql->executeRequete("update article set categorie = '$_POST[categorie_article]' where idArticle = $_GET[id_article]");
-            echo '<p class = validation>validation ok pour '.$_POST['categorie_article'].'</p>';
-        }
-        if(!empty($_FILES['image_article'])) {
-            $nom_article = $fonction_sql->executeRequete("SELECT nom from article where idArticle = $_GET[id_article]");
-            $nom_article = $nom_article->fetch_assoc();
-            // $fonction_sql->debug($nom_article['nom']);
-            echo '<p class="validation">' .$nom_article['nom'].'</p>';
-            $dossier = '../inc/img/photos_articles/';
-            $taille_maxi = 1000000000;
-            $taille = filesize($_FILES['image_article']['tmp_name']);
-            $extensions = array('.png', '.gif', '.jpg', '.jpeg');
-            $extension = strrchr($_FILES['image_article']['name'], '.'); 
-            //Si l'extension n'est pas dans le tableau
-            if(!in_array($extension, $extensions)) {
-                $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
-            }
-            //si taille du fichier grande
-            if($taille>$taille_maxi) {
-                $erreur = 'Le fichier est trop gros...';
-            }
-            //S'il n'y a pas d'erreur
-            if(!isset($erreur)) {
-                $fichier =  $nom_article['nom'].$extension;
-                //On essaye de deplacer le fichier dans notre repertoire
-                //si cela nous donne FALSE, cela n'a pas fonctionne.
-                if(!move_uploaded_file($_FILES['image_article']['tmp_name'], $dossier . $fichier)) {
-                    echo '<p class="erreur">Echec de l\'upload !</p>';
-                    echo "Not uploaded because of error #".$_FILES["image_article"]["error"];
-                }
-                else {
-                    $fonction_sql->executeRequete("update photo set cheminPhoto = '$dossier$fichier' where idPhoto =(SELECT idPhoto from affiche where idArticle = $_GET[id_article] )");
-                    echo '<p class = validation>validation ok pour files.</p>';
-                }
-            }
-        }
-        echo("<meta http-equiv='refresh' content='1'>");
-    }
+    
 }
